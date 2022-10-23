01d46bfd239330a0c03f0940d86592735e4efd0a
chiashenglin <bryan2160088@gmail.com>
2022-10-22 23:59:12 -0700

    first commit!

--- 1-scraper/playwright_demo.py
+++ 1-scraper/playwright_demo.py
@@ -1,9 +1,17 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+url = 'https://www.rew.ca/'
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=500)
     page = browser.new_page()
-    page.goto('https://google.com')
-
-    
+    page.goto(url)
+    page.fill('input#listing_search_query','Vancouver')
+    page.click('button[type=submit]')
+    # page.is_visible('div.displaypanel-price')
+    html = page.inner_html('div.col-xs-12.col-md-8')
+    #print(html)
+    page.click('a[href*="properties"]')
+    page.is_visible('div.sectionblock')
+    html1 = page.inner_html('div.sectionblock')
+    print(html1)
