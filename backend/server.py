c81ce496350b32dfd0a6fb88442462453f828110
chiashenglin <bryan2160088@gmail.com>
2023-02-05 15:11:35 -0800

    add backend MongoDB database and feature for file to db

---
+++ backend/server.py
@@ -0,0 +1,102 @@
+from flask import Flask, Response, request
+import pymongo
+import json
+from bson.objectid import ObjectId
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host='localhost', 
+        port=27017,
+        serverSelectionTimeoutMS=1000
+    )
+    db = mongo.company
+    mongo.server_info() # trigger exception if cannot connect to db
+except:
+    print('ERROR - Cannot connect to db')
+################################
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        user = {
+            'name':request.form['name'], 
+            'lastName':request.form['lastName']
+        }
+        dbResponse = db.users.insert_one(user)
+        for attr in dir(dbResponse.inserted_id):
+            print(attr)
+        return Response(
+            response=json.dumps({
+                "message":"user created successfully",
+                'id':f'{dbResponse.inserted_id}',
+            }),
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as ex:
+        print('ERROR - Cannot create user')
+        print(ex)
+
+################################
+@app.route('/users', methods=['GET'])
+def get_user():
+    try:
+        data = list(db.users.find())
+        for user in data:
+            user['_id'] = str(user['_id'])
+
+        return Response(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response=json.dumps({
+                "message":"cannot get user",
+                
+            }),
+            status=500,
+            mimetype='application/json'
+        )
+################################
+@app.route("/users/<id>", methods=['PATCH'])
+def update_user(id):
+    try:
+        dbResponse = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {"$set":{"name":request.form["name"]}}
+        )
+        for attr in dir(dbResponse):
+            print(f'{attr}')
+        
+        if (dbResponse.modified_count == 1):
+            return Response(
+                response=json.dumps({'message':'good'}),
+                status=200,
+                mimetype='application/json'
+            )
+        else:
+            return Response(
+                response=json.dumps({'message':'nothing to update'}),
+                status=200,
+                mimetype='application/json'
+            )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response=json.dumps({
+                "message":"cannot update user",
+            }),
+            status=500,
+            mimetype='application/json'
+        )
+################################
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteuser(id):
+    db.users.delete_one({'_id':"Maki"})
+
+################################
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
